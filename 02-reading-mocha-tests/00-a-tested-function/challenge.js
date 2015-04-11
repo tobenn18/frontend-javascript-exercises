922e75d3886e7605c2fc6b319e0fa4a48a939a21
Ben Glumack <bglumack@hotmail.com>
2015-04-11 16:50:06 +0000

    mocha test

--- 02-reading-mocha-tests/00-a-tested-function/challenge.js
+++ 02-reading-mocha-tests/00-a-tested-function/challenge.js
@@ -1,4 +1,4 @@
 module.exports.linkTo = function(text, address) {   
   return "<a href='" + address + "'>" + text + "</a>"
 };
-module.exports.linkTo("Bloc","http://www.bloc.io")
+module.exports.linkTo("Bloc","http://www.bloc.io");
