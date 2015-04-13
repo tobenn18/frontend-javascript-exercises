c73fbe16dddf739d4123bf490dcf7490bc62f92f
Ben Glumack <bglumack@hotmail.com>
2015-04-13 19:45:20 +0000

    05-00Basics

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -2,16 +2,13 @@ module.exports.equalStrings = function(stringOne, stringTwo) {
     return stringOne === stringTwo;
     
 };
-
 module.exports.notEqual = function(one, two) {
     return one !== two;
         };
-
 module.exports.inBetween = function(lower, middle, upper) {
     return lower < middle && middle < upper;
             
 };
-
 module.exports.outsideRanges = function(number) {
     var inRange = (10 <= number && number <= 20)
                 ||(42 < number && number <= 75) 
@@ -19,7 +16,6 @@ module.exports.outsideRanges = function(number) {
                 ;
         return !inRange;
 };
-
 module.exports.nameAndPrice = function(name, price) {
     return (name === 'NYTimes' || name === 'LATimes') && (price >= 1);
 };
