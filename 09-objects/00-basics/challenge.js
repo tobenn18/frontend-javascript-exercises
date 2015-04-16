5e375e2f0072f19a6db05c883a87ee25bef2037e
Ben Glumack <bglumack@hotmail.com>
2015-04-16 20:33:02 +0000

    objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,30 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(courseTitle,courseDuration,courseStudents){
+         var myCourse = {
+            title : courseTitle,
+            duration : courseDuration,
+            students : courseStudents
+        };
+        return myCourse;
+     };
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object,newProp,newValue){
+    if(object[newProp] === undefined){
+        object[newProp] = newValue;
+    }
+    return object;
+};
 
-module.exports.formLetter = undefined;
+module.exports.formLetter = function(letter){
+  return "Hello " + letter.recipient + ",\n\n" + letter.msg + "\n\nSincerely,\n" + letter.sender;  
+    };
 
-module.exports.canIGet = undefined;
+module.exports.canIGet = function(item,money){
+    var price = {
+        'MacBook Air' : 999,
+        'MacBook Pro' : 1299,
+        'Mac Pro'     : 2499,
+        'Apple Sticker' : 1
+    };
+    product = price[item];
+    return (price !== undefined && money >= product);
+};
