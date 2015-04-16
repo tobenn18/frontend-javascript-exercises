a8ec85352e6d79d8f74bd6efbe6bee18db45d928
Ben Glumack <bglumack@hotmail.com>
2015-04-16 17:24:23 +0000

    arrays

--- 08-arrays/02_Grocery_List/challenge.js
+++ 08-arrays/02_Grocery_List/challenge.js
@@ -1,3 +1,10 @@
-module.exports.addItem = undefined;
+module.exports.addItem = function(item,list){
+    if(item.indexOf(list) == - 1){
+        list.push(item);
+    }
+    return list;
+};
 
-module.exports.reverseSortedList = undefined;
+module.exports.reverseSortedList = function(list){
+    return list.sort().reverse();
+    };
