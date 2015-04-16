582e5e8fd7f526e68a9c40c89276ea3dd030b241
Ben Glumack <bglumack@hotmail.com>
2015-04-16 17:28:02 +0000

    array 00 01'

--- 08-arrays/00_Array_Definition/challenge.js
+++ 08-arrays/00_Array_Definition/challenge.js
@@ -1,3 +1,10 @@
-module.exports.newArray = undefined;
+module.exports.newArray = function(a,b,c,d){
+     return newArray = [a,b,c,d];
+};
+module.exports.newArray(1,2,3,4);
 
-module.exports.firstAndLast = undefined;
+module.exports.firstAndLast = function(a){
+    var last = a.length - 1;
+    return [a[0],a[last]]; 
+};
+//
--- 08-arrays/01_Array_Functions/challenge.js
+++ 08-arrays/01_Array_Functions/challenge.js
@@ -1,5 +1,12 @@
-module.exports.reversePlusOne = undefined;
+module.exports.reversePlusOne = function(a){
+    a.reverse()
+    a.unshift(1);
+        return a;
+    };
 
-module.exports.plusesEverywhere = undefined;
+module.exports.plusesEverywhere = function(a){
+    
+    return a.join("+");
+};
 
 module.exports.arrayQuantityPlusOne = undefined;
