5ef1a8673e55c09c158204acc134656e9aec0626
Ben Glumack <bglumack@hotmail.com>
2015-04-21 20:25:52 +0000

    10 Loops 02

--- 10-loops/02-object-operations/challenge.js
+++ 10-loops/02-object-operations/challenge.js
@@ -1,5 +1,23 @@
-module.exports.copy = undefined;
+module.exports.copy = function(object){
+    newObj = {};
+    for(var keys in object){
+        newObj[keys] = object[keys];
+    }
+    return newObj;
+};
 
-module.exports.extend = undefined;
+module.exports.extend = function(dest,src){
+    for(value in src){
+        dest[value] = src[value];
+    };
+    return dest;
+};
 
-module.exports.hasElems = undefined;
+module.exports.hasElems = function(obj,array){
+    for(var i; i < obj.length; i++){
+        if(obj[array[i]] === undefined){
+            return false;
+        }
+    }
+    return true;
+};
